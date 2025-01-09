3d74a4e7247789f1964b5e194bc9d09a1760d49a
Luiz <luizinhon@hotmail.com>
2025-01-09 01:46:52 -0300

    cleanup: cart-total component

--- src/components/cart/cart-total.tsx
+++ src/components/cart/cart-total.tsx
@@ -16,67 +16,64 @@ export default function CartTotal({ list, onDelete }: ICartTotal) {
 	React.useEffect(() => {
 		setTotal(() => {
 			const total = [0, 0, 0];
-			list.map((item) => {
+			list.forEach((item) => {
 				if (item.type === 'Champion' && !item.disabledPrice.BE)
 					total[0] += item.discountBE.newPrice;
 				if (item.type === 'Champion' && !item.disabledPrice.RP)
 					total[2] += item.discountRP.newPrice;
 				if (item.type === 'Skin' && !item.disabledPrice.RP)
 					total[2] += item.discountRP.newPrice;
-				if (item.type === 'Skin' && !item.disabledPrice.OE) {
+				if (item.type === 'Skin' && !item.disabledPrice.OE)
 					total[1] += item.cost.orangeEssence;
-				}
 			});
 			return total;
 		});
 	}, [list]);
 
-	if (!list) return null;
-	if (total)
-		return (
-			<div className={`${styles.total} animeTopBottom`}>
-				<h3>Totais</h3>
-				<ul>
-					<li>
-						<p>Total de essência azul</p>
-						<span>
-							<Image
-								src={'/assets/be.svg'}
-								alt='Blue essence'
-								width={17}
-								height={20}
-							/>
-							{total[0]}
-						</span>
-					</li>
-					<li>
-						<p>Total de essência laranja</p>
-						<span>
-							<Image
-								src={'/assets/oe.svg'}
-								alt='Orange essence'
-								width={15}
-								height={20}
-							/>
-							{total[1]}
-						</span>
-					</li>
-					<li>
-						<p>Total de riot points</p>
-						<span>
-							<Image
-								src={'/assets/rp.svg'}
-								alt='Riot points'
-								width={20}
-								height={20}
-							/>
-							{total[2]}
-						</span>
-					</li>
-				</ul>
-				<button className='button' onClick={onDelete}>
-					Remover tudo
-				</button>
-			</div>
-		);
+	return (
+		<div className={`${styles.total} animeTopBottom`}>
+			<h3>Totais</h3>
+			<ul>
+				<li>
+					<p>Total de essência azul</p>
+					<span>
+						<Image
+							src={'/assets/be.svg'}
+							alt='Blue essence'
+							width={17}
+							height={20}
+						/>
+						{total[0]}
+					</span>
+				</li>
+				<li>
+					<p>Total de essência laranja</p>
+					<span>
+						<Image
+							src={'/assets/oe.svg'}
+							alt='Orange essence'
+							width={15}
+							height={20}
+						/>
+						{total[1]}
+					</span>
+				</li>
+				<li>
+					<p>Total de riot points</p>
+					<span>
+						<Image
+							src={'/assets/rp.svg'}
+							alt='Riot points'
+							width={20}
+							height={20}
+						/>
+						{total[2]}
+					</span>
+				</li>
+			</ul>
+			<button className='button' onClick={onDelete}>
+				Remover tudo
+			</button>
+		</div>
+	);
 }
